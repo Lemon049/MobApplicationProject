5e72f75e0d9505a13769c286d4d12f1c2f8d5a9c
Lemon049 <yywmail5@gmail.com>
2024-12-06 11:42:32 +0100

    Fragments sample

--- app/src/main/java/com/example/mobaplicationproject/CredentialsManager.kt
+++ app/src/main/java/com/example/mobaplicationproject/CredentialsManager.kt
@@ -5,7 +5,19 @@ class CredentialsManager {
     val userData = mutableMapOf<String, String>()
 
     fun isEmailValid(email: String): Boolean {
-        return email.contains("@") && email.contains(".") && email.length > 5
+
+        if (email.isEmpty()) return false
+
+        val emailPattern = ("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+")
+        val regex = Regex(emailPattern)
+
+        return regex.matches(email)
     }
 
     fun isPasswordValid(password: String): Boolean {
