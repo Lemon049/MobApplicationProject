0a9c9831ebc864d4f1c251378448fce04edc2e15
lytvynov-ihor <ltv.ihor@gmail.com>
2024-12-21 17:57:28 +0100

    Commit1

--- app/src/main/java/com/example/mobaplicationproject/activity_with_fragment.kt
+++ app/src/main/java/com/example/mobaplicationproject/activity_with_fragment.kt
@@ -1,11 +1,14 @@
 package com.example.mobaplicationproject
 
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class activity_with_fragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,37 +20,24 @@ class activity_with_fragment : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val fragmentA = FragmentA()
-        val fragmentB = FragmentB()
-        val buttonFragment = findViewById<Button>(R.id.btnFragment)
-        var isActive : Boolean = false
 
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFragment, fragmentA)
-            commit()
-            isActive = true
+        // Set the initial fragment
+        supportFragmentManager.commit {
+            replace<FragmentB>(R.id.flFragment)
         }
+    }
 
-        buttonFragment.setOnClickListener{
-            if (isActive == true)
-            {
-                supportFragmentManager.beginTransaction().apply {
-                    replace(R.id.flFragment, fragmentB)
-                    commit()
-                    isActive = false
-                }
-            }
-            else
-            {
-                supportFragmentManager.beginTransaction().apply {
-                    replace(R.id.flFragment, fragmentA)
-                    commit()
-                    isActive = true
-                }
-            }
-
-
+    fun switchToRegisterFragment() {
+        supportFragmentManager.commit {
+            replace<Fragment>(R.id.flFragment)
+            addToBackStack(null) // Enable back navigation
         }
+    }
 
+    fun switchToLoginFragment() {
+        supportFragmentManager.commit {
+            replace<FragmentB>(R.id.flFragment)
+            addToBackStack(null) // Enable back navigation
+        }
     }
 }
